0529a502d79e249af0bc80bbd2cf36b92af6f875
William Burke <burke24@gmail.com>
2020-07-27 20:33:08 -0700

    Initial commit

---
+++ R/fetchinfo.R
@@ -0,0 +1,46 @@
+#' fetchinfo
+#'
+#' Outputs info on R session, host machine, and pretty picture
+#' @author William Burke
+#' @export
+
+fetchinfo = function() {
+
+  cpuinfo = system("wmic cpu get maxclockspeed, name, ThreadCount, numberofcores",intern = T)
+  cpuinfo = trimws(substr(cpuinfo[2], 0, nchar(cpuinfo[2]) - 2))
+  cpuinfo = unlist(strsplit(cpuinfo, "  +"))
+  cpucore = paste0(cpuinfo[3],"(",cpuinfo[4],")")
+
+  catout = c(
+    "\n",
+    "\t", "ooooooooo.   ", "      OS:", Sys.info()[c(1,2)], "\n",
+    "\t", "888   `Y88.  ", "Pkg libs:", path.expand(Sys.getenv("R_LIBS")), "\n",
+    "\t", "888   .d88'  ", "Computer:", Sys.info()[4], "\n",
+    "\t", "888ooo88P'   ", "    User:", Sys.info()[8], "\n",
+    "\t", "888`88b.     ", "     CPU:", cpuinfo[2], "\n",
+    "\t", "888  `88b.   ", "   Cores:", cpucore, "\n",
+    "\t", "o888o  o888o ", "\n"
+  )
+
+  cat(catout)
+
+# ooooooooo.
+# 888   `Y88.
+# 888   .d88'
+# 888ooo88P'
+# 888`88b.
+# 888  `88b.
+# o888o  o888o
+
+#   8888888b.
+#   888   Y88b
+#   888    888
+#   888   d88P
+#   8888888P"
+#   888 T88b
+#   888  T88b
+#   888   T88b
+
+
+
+}
---
+++ README.md
@@ -0,0 +1,41 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# fetchinfo
+
+<!-- badges: start -->
+
+<!-- badges: end -->
+
+Idk, it’s like neofetch but for R and bad.
+
+## Installation
+
+``` r
+devtools::install_github(repo = "wburke24/fetchinfo")
+```
+
+<!-- You can install the released version of fetchinfo from [CRAN](https://CRAN.R-project.org) with: -->
+
+<!-- ``` r -->
+
+<!-- install.packages("fetchinfo") -->
+
+<!-- ``` -->
+
+## Example
+
+Code goes beep boop
+
+``` r
+library(fetchinfo)
+fetchinfo()
+#> 
+#>       ooooooooo.          OS: Windows 10 x64 
+#>       888   `Y88.   Pkg libs: C:/Users/burke/Documents/R/library 
+#>       888   .d88'   Computer: WILL-DESKTOP 
+#>       888ooo88P'        User: burke 
+#>       888`88b.           CPU: Intel(R) Core(TM) i7-8700K CPU @ 3.70GHz 
+#>       888  `88b.       Cores: 6(12) 
+#>       o888o  o888o
+```
